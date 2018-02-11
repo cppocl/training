e51e60d7657c6b94ff7a1914f0dd1e67fa7568b8
colinrg <colingirling@googlemail.com>
2018-02-11 19:08:13 +0000

    Added some information about stack and head memory address space and added new/delete for a class object.

--- Lesson 022 - classes/Lesson 022 - classes.cpp
+++ Lesson 022 - classes/Lesson 022 - classes.cpp
@@ -28,6 +28,11 @@ int main()
     // Create a variable that is of a Person type.
     // This is similar to the struct example in an earlier lesson,
     // but with the addition of having functions on the object we can use.
+    // Notice we create the person object in a similar way to creating an int or bool.
+    // Variables created like person, int, bool, etc are created in memory with the stack space.
+    // This stack space in memory is faster than dynamically allocated memory we have already
+    // seen when using new and delete, and when a variable on the stack is not longer usable,
+    // it is automatically released back into the free memory space.
     Person person;
 
     // We can call the various set functions on the person object to
@@ -54,6 +59,25 @@ int main()
     Person* person_ptr = &person;
     std::cout << person_ptr->GetFirstName() << " " << person_ptr->GetLastName();
     std::cout << " was born on " << out_day << "/" << out_month << "/" << year << std::endl;
+
+    // It's also possible to create a class dynamically in memory, just like we saw in
+    // the advanced pointers lessons.
+    // Dynamic memory actually exists in an area of memory known as the heap.
+    // Any memory dynamically allocated also needs to be manually freed.
+    person_ptr = new Person();
+    person_ptr->SetFirstName("Jane");
+    person_ptr->SetLastName("Smithers");
+    person_ptr->SetDateOfBirth(21, 06, 1990);
+    person_ptr->GetDateOfBirth(day, month, year);
+    out_day = static_cast<std::uint16_t>(day);
+    out_month = static_cast<std::uint16_t>(month);
+
+    std::cout << person_ptr->GetFirstName() << " " << person_ptr->GetLastName();
+    std::cout << " was born on " << out_day << "/" << out_month << "/" << year << std::endl;
+
+    // And just like we had to free the allocated memory in the advanced pointers lesson,
+    // we need to do the same for person_ptr.
+    delete person_ptr;
 
     return 0;
 }
